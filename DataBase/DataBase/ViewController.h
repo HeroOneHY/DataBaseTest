10b8a485fe5cce8c44302fb78a4bede43dedf721
yanhe <yanhe@360.cn>
2018-04-02 12:56:27 +0800

    初始化项目

---
+++ DataBase/DataBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataBase
+//
+//  Created by yanhe on 2018/4/2.
+//  Copyright © 2018年 yanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataBase/DataBase/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DataBase
+//
+//  Created by yanhe on 2018/4/2.
+//  Copyright © 2018年 yanhe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
